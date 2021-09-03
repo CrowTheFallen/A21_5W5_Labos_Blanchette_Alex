c36ae3483da6cdfaa705bec0d4ef3e786c979b4e
Alex Blanchette <bigue1994@hotmail.com>
2021-09-03 17:47:43 -0400

    Création de la librairie et de l'ajout de la connexion string

--- A21_5W5_Laboratoires-S02_DataLayers_RepoPatterns/Jungle/Jungle/Startup.cs
+++ A21_5W5_Laboratoires-S02_DataLayers_RepoPatterns/Jungle/Jungle/Startup.cs
@@ -1,6 +1,8 @@
+using JungleDataAcess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,10 @@ namespace Jungle
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllersWithViews();
+
+
+      services.AddDbContext<JungleDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ A21_5W5_Laboratoires-S02_DataLayers_RepoPatterns/Jungle/JungleDataAcess/JungleDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace JungleDataAcess
+{
+    public class JungleDbContext : DbContext
+    {
+        public JungleDbContext(DbContextOptions<JungleDbContext> options) : base(options)
+        {
+
+        }
+
+
+
+    }
+}
