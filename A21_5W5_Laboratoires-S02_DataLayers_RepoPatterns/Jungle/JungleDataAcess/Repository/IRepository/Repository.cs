ac92add1c1deae284eed1e166359943f8e227ec0
Alex Blanchette <bigue1994@hotmail.com>
2021-09-06 15:00:18 -0400

    Étape après le : CONFIGURER LE UNITOFWORK
    Ajout du fichier Repository et de tous les autres classes et interfaces

---
+++ A21_5W5_Laboratoires-S02_DataLayers_RepoPatterns/Jungle/JungleDataAcess/Repository/IRepository/Repository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JungleDataAcess.Repository.IRepository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null, bool isTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null, bool isTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ A21_5W5_Laboratoires-S02_DataLayers_RepoPatterns/Jungle/JungleDataAcess/Repository/IRepository/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JungleDataAcess;
+
+namespace JungleDataAcess.Repository.IRepository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly JungleDbContext _db;
+
+        public UnitOfWork(JungleDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
