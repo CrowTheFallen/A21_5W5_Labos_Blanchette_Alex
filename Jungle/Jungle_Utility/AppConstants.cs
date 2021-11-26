36485d82bf0ce3bf6ca0e7baf46c8029364a5e9b
Alex Blanchette <bigue1994@hotmail.com>
2021-11-26 15:13:01 -0500

    push ini

--- Jungle/Jungle_Utility/AppConstants.cs
+++ Jungle/Jungle_Utility/AppConstants.cs
@@ -2,13 +2,19 @@
 
 namespace Jungle_Utility
 {
-  public class AppConstants
-  {
-    // Gestion des images
-    public static string ImagePathBooks = @"\images\Destination\";
-    public static string ImagePathBooksView = @"/images/Destination/";
+    public class AppConstants
+    {
+        // Gestion des images
+        public static string ImagePathBooks = @"\images\Destination\";
+        public static string ImagePathBooksView = @"/images/Destination/";
+
+        // Roles et Administrateur
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
+        //Autres Rôles
+
+        public const string EmailAdmin = "valerie.turgeon@cegepmontpetit.ca";
 
 
-
-  }
+    }
 }
